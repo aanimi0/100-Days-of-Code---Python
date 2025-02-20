fc56a7d7c3973f4476c360da6a6885bb5e7b558a
aanimi0 <alexnimirceag@gmail.com>
2025-02-20 11:15:34 +0200

    Day 9 done

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,35 @@
+#print("\n" * 100) #to clear the screen
+from art import logo
+print(logo)
 # TODO-1: Ask the user for input
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
+def find_highest_bidder(bidding_dictionary):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+
+    print(f"Winner is {winner} with the bid {highest_bid}")
+
+
+game_ongoing = True
+bids = {}
+
+while game_ongoing:
+
+    user_name = input("Please type your name\n")
+    user_bid = int(input("Please type the amount: $"))
+    bids[user_name] = user_bid
+    check_continue = input("Do you want to continue yes or no?\n")
+    if check_continue == 'no':
+        game_ongoing = False
+        find_highest_bidder(bids)
+    else:
+        print("\n" * 20)
 
 
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,32 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+                         }
+print(programming_dictionary["Bug"])
+
+programming_dictionary["Loop"] = "the action of doing something over and over"
+empty_dictionary ={}
+print(programming_dictionary)
+
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+student_grades = {}
+
+for key in student_scores:
+    value_to_check = student_scores[key]
+    if value_to_check < 70:
+        student_grades[key] = "Fail"
+    elif 70 < value_to_check <= 80:
+        student_grades[key] = "Acceptable"
+    elif 80 < value_to_check <= 90:
+        student_grades[key] = "Exceeds Expectations"
+    elif 90 < value_to_check <= 100:
+        student_grades[key] = "Outstanding"
+
+print(student_grades)
+print(student_scores)
